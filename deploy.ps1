941b2f6c1282be40ff02e28af879239c0e333b2b
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-06-19 18:53:39 +0100

    jester update bootstrap

---
+++ deploy.ps1
@@ -0,0 +1,22 @@
+# powershell script to deploy the project
+
+# store current path for later use
+$ProjectRootPath = Get-Location
+
+# build old.yayjaybae.com
+cd "$ProjectRootPath\old.yayjaybae.com"
+npm install
+npm run build
+
+# copy build files to ASSETS directory
+xcopy "$ProjectRootPath\old.yayjaybae.com\out" "$ProjectRootPath\ASSETS\old.yayjaybae.com" /E /I /Y
+
+# build yayjaybae.com
+echo "Nothing to build for yayjaybae.com, skipping build step."
+
+# copy build files to ASSETS directory
+xcopy "$ProjectRootPath\yayjaybae.com" "$ProjectRootPath\ASSETS\yayjaybae.com" /E /I /Y
+
+# return to root and deploy project
+cd $ProjectRootPath
+npx wrangler deploy
---
+++ server/index.js
@@ -0,0 +1,140 @@
+const configuration = {
+  redirects: [
+    // x / twitter
+    { from: "/x", to: "https://x.com/yayjaybae" },
+    { from: "/twitter", to: "/x" },
+
+    // twitch
+    { from: "/twitch", to: "https://www.twitch.tv/yayjaybae" },
+    { from: "/live", to: "/twitch" },
+    { from: "/ttv", to: "/twitch" },
+
+    // youtube
+    { from: "/youtube", to: "https://www.youtube.com/@yayjaybae" },
+
+    // ko-fi
+    { from: "/kofi", to: "https://ko-fi.com/yayjaybae" },
+    { from: "/shop", to: "https://ko-fi.com/yayjaybae/shop" },
+
+    // throne
+    { from: "/throne", to: "https://throne.com/yayjaybae/wishlist" },
+
+    // discord
+    { from: "/discord", to: "https://discord.gg/UMcgX2C6Pd" },
+
+    // instagram
+    { from: "/instagram", to: "https://www.instagram.com/yayjaybae" },
+    { from: "/insta", to: "/instagram" },
+
+    // tiktok
+    { from: "/tiktok", to: "https://www.tiktok.com/@yayjaybae" },
+
+    // bluesky
+    { from: "/bluesky", to: "https://bsky.app/profile/yayjaybae.com" },
+    { from: "/bsky", to: "/bluesky" },
+
+    // dubby
+    {
+      from: "/dubby",
+      to: "https://dubby.gg/discount/yayjaybae?redirect=/products/dubby-air-freshener-2-pack",
+    },
+
+    // somatic
+    { from: "/somatic", to: "https://5938bf-3.myshopify.com/yayjaybae" },
+
+    // hytale
+    { from: "/hytale", to: "https://www.hytale.com/" },
+
+    // steam
+    {
+      from: "/steam",
+      to: "https://store.steampowered.com/wishlist/id/YayJayBae/",
+    },
+
+    // vrchat
+    { from: "/vrchat", to: "https://vrc.group/YAYJAY.9440" },
+    { from: "/vrc", to: "/vrchat" },
+
+    // fourthwall
+    { from: "/fourthwall", to: "https://fourthwall.yayjaybae.com" },
+    { from: "/store", to: "/fourthwall" },
+  ],
+};
+
+export default {
+  /*
+    Fetch event handler, this function will be called whenever a request is made to the worker.
+    The function will parse the request and return a response based on the request path.
+
+    @param {Request} request - the incoming request object
+    @param {Environment} environment - the environment object
+    @param {Context} context - the context object
+
+    @returns {Response} a new Response object
+  */
+  async fetch(request, environment, context) {
+    const url = new URL(request.url);
+
+    if (configuration.redirects) {
+      for (const redirect of configuration.redirects) {
+        if (
+          url.pathname === redirect.from ||
+          url.pathname === redirect.from + "/"
+        ) {
+          return Response.redirect(redirect.to, 301);
+        }
+      }
+    }
+
+    if (url.pathname.startsWith("/api")) {
+      return new Response("Hello from the API!", {
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
+
+    // internal rewrite hostname dependent
+    switch (url.hostname) {
+      case "yayjaybae-com.jaylaoftheday.workers.dev":
+      case "www.yayjaybae.com":
+      case "old.yayjaybae.com":
+      case "yayjaybae.com":
+        url.pathname = "/old.yayjaybae.com" + url.pathname;
+        return environment.ASSETS.fetch(new Request(url, request));
+      case "new.yayjaybae.com": {
+        url.pathname = "/yayjaybae.com" + url.pathname;
+        return environment.ASSETS.fetch(new Request(url, request));
+      }
+    }
+
+    // default to 404 if no matching hostname is found
+    return new Response("404 Not Found", { status: 404 });
+  },
+
+  /*
+    Email event handler, this function will be called whenever an email is sent to the worker.
+    The function will parse the email message and forward it to a specified email address.
+
+    @param {Message} message - the incoming email message object
+    @param {Environment} environment - the environment object
+    @param {Context} context - the context object
+
+    @returns {void}
+  */
+  async email(message, environment, context) {
+    message.forward("catchall@yayjaybae.com");
+  },
+
+  /*
+    Scheduled event handler, this function will be called whenever a scheduled event is triggered.
+    The function will perform a task and return a response based on the task outcome.
+
+    @param {Event} event - the incoming event object
+    @param {Environment} environment - the environment object
+    @param {Context} context - the context object
+
+    @returns {void}
+  */
+  async scheduled(event, environment, context) {
+    console.log("Scheduled event triggered!");
+  },
+};
