68dff7fc8b10a722d02a1436c48f2a304569596c
Alex Pooley (@zuedev) <zuedev@gmail.com>
2026-07-12 17:01:42 +0100

    update credits

--- frontend/src/app/credits/page.js
+++ frontend/src/app/credits/page.js
@@ -1,152 +1,344 @@
 export default () => {
-  let artCredits = [
+  let creditSections = [
     {
-      thing: "1.0 VTuber Model Art + Rig",
-      artist: "COGNITOHAZARD",
-      link: "https://linktr.ee/Hackneid",
+      title: "Jester Credits",
+      credits: [
+        {
+          thing: "Jester 3D Vtuber Models",
+          artists: [
+            { name: "Korunmula", link: "https://korunmula.carrd.co/" },
+          ],
+        },
+        {
+          thing: "Jester 3D Model ARKit",
+          artists: [{ name: "Tofu_Dog", link: "https://tofudog.carrd.co/" }],
+        },
+        {
+          thing: "Chibi Jester Jay Model",
+          artists: [
+            {
+              name: "BumantaraStudio",
+              link: "https://vgen.co/BumantaraStudio",
+            },
+          ],
+        },
+        {
+          thing: "Banner Art",
+          artists: [
+            { name: "WIKIPIXEL", link: "https://wikipixel.carrd.co/" },
+          ],
+        },
+        {
+          thing: "Emotes",
+          artists: [
+            {
+              name: "EightyEightDesign",
+              link: "https://vgen.co/EightyEightDesign",
+            },
+            {
+              name: "DuggerJuggernaut",
+              link: "https://vgen.co/duggerjuggernaut",
+            },
+            { name: "SmallzVT", link: "https://vgen.co/SmallzVT" },
+            {
+              name: "NocturneAubade",
+              link: "https://nocturneaubade.carrd.co/",
+            },
+            { name: "haetchi", link: "https://vgen.co/haetchi" },
+            { name: "BeaGremlin", link: "https://beagremlin.carrd.co/" },
+            { name: "GoldCanines", link: "https://goldcanines.carrd.co/" },
+            { name: "EmoteLab", link: null },
+          ],
+        },
+        {
+          thing: "Raid Alert Animation",
+          artists: [
+            { name: "Karutaproject", link: "https://vgen.co/Karutaproject" },
+          ],
+        },
+        {
+          thing: "Raid Alert Art",
+          artists: [
+            { name: "naaaaaaco369", link: "https://vgen.co/naaaaaaco369" },
+            { name: "WIKIPIXEL", link: "https://wikipixel.carrd.co/" },
+          ],
+        },
+        {
+          thing: "All Other Alerts",
+          artists: [{ name: "EmoteLab", link: null }],
+        },
+        {
+          thing: "Stinger",
+          artists: [{ name: "Gishi", link: "https://vgen.co/Gishimon" }],
+        },
+        {
+          thing: "Starting Soon/Stream Ending Animation",
+          artists: [{ name: "11khwnn", link: "https://vgen.co/11khwnn" }],
+        },
+        {
+          thing: "Be Right Back/Game Loading Screen",
+          artists: [{ name: "Kryban", link: "https://vgen.co/kryban" }],
+        },
+        {
+          thing: "Pattern Background",
+          artists: [{ name: "Romsesito", link: "https://vgen.co/romsesito" }],
+        },
+        {
+          thing:
+            "Logo, Backgrounds, Overlays, Panels, Showcase, Schedule, Chat Widget Design, Screens",
+          artists: [{ name: "Anya", link: "https://vgen.co/anyablueberry" }],
+        },
+        {
+          thing: "Badges",
+          artists: [
+            {
+              name: "JackGravitty",
+              link: "https://manylink.co/@JackGravitty",
+            },
+            { name: "Anya", link: "https://vgen.co/anyablueberry" },
+          ],
+        },
+        {
+          thing: "Jester Merch Art",
+          artists: [
+            { name: "naco", link: "https://vgen.co/naaaaaaco369" },
+            { name: "Rezami", link: "https://vgen.co/Rezami" },
+            { name: "BLEEPBLEEP", link: "https://vgen.co/BLEEPPBLEEPP" },
+          ],
+        },
+        {
+          thing: "Trixster Lineart",
+          artists: [
+            { name: "Little_Dismay", link: "https://vgen.co/Little_Dismay" },
+          ],
+        },
+        {
+          thing: "Original Music and Sub Alert Sounds",
+          artists: [
+            {
+              name: "Sundaiiz",
+              link: "https://bsky.app/profile/sundaiiz.kawaii.social",
+            },
+          ],
+        },
+        {
+          thing: "Jester Design",
+          artists: [
+            {
+              name: "PantherPaintz",
+              link: "https://www.instagram.com/pantherpaintz/",
+            },
+          ],
+        },
+        {
+          thing: "Jester Reference PNGs",
+          artists: [
+            { name: "CielCinna", link: "https://cielcinna.carrd.co/" },
+          ],
+        },
+        {
+          thing: "Jester Reference Sheet Formatting and Additions",
+          artists: [{ name: "SmallzVT", link: "https://vgen.co/SmallzVT" }],
+        },
+      ],
     },
     {
-      thing: "2.0 VTuber Model Art + Rig",
-      artist: "Qwerty_Soda",
-      link: "https://linktr.ee/qwertysoda",
-    },
-    {
-      thing: "2.0 Model VBridger Enhancements",
-      artist: "DesdemonaVT",
-      link: "https://twitter.com/DesdemonaVT",
-    },
-    {
-      thing: "3D VTuber Models",
-      artist: "Birdchii",
-      link: "https://x.com/birdchii",
-    },
-    {
-      thing: "Edits to 3D Model",
-      artist: "Violet_Viper",
-      link: "https://violetviper.carrd.co/",
-    },
-    {
-      thing: "Banner Art",
-      artist: "dormirVT",
-      link: "https://twitter.com/dormirVT",
-    },
-    {
-      thing: "Emotes",
-      artist: "EightyEightDesign",
-      link: "https://vgen.co/EightyEightDesign",
-    },
-    {
-      thing: "Emotes",
-      artist: "Alek",
-      link: "https://twitter.com/Alek0l6",
-    },
-    {
-      thing: "Emotes",
-      artist: "Teresa Ellayne",
-      link: "https://teresaellayne.com/",
-    },
-    {
-      thing: "Emotes",
-      artist: "NocturneAubade",
-      link: "https://nocturneaubade.carrd.co/",
-    },
-    {
-      thing: "Emotes",
-      artist: "BeaGremlin",
-      link: "https://beagremlin.carrd.co/",
-    },
-    {
-      thing: "Emotes",
-      artist: "GoldCanines",
-      link: "https://goldcanines.carrd.co/",
-    },
-    {
-      thing: "Sub Badges",
-      artist: "JackGravitty",
-      link: "https://manylink.co/@JackGravitty",
-    },
-    {
-      thing: "Panel Art",
-      artist: "AmaiMilk",
-      link: "https://twitter.com/AmaiMilk69",
-    },
-    {
-      thing: "Logo",
-      artist: "Baebiiverse",
-      link: "https://www.fiverr.com/baebiiverse",
-    },
-    {
-      thing: "Keychain and Sticker Art",
-      artist: "kd_0510",
-      link: "https://kdtk0510.wixsite.com/mysite",
-    },
-    {
-      thing: "Chatting and Gaming Background",
-      artist: "yuzuyukihira",
-      link: "https://vgen.co/yuzuyukihira",
-    },
-    {
-      thing: "Starting and Ending Screens Art",
-      artist: "nostdoro",
-      link: "https://nostee.com/",
-    },
-    {
-      thing: "Starting and Ending Screens Animation",
-      artist: "ClericalEric",
-      link: "https://www.twitch.tv/clericaleric",
-    },
-    {
-      thing: "Be Right Back Screen",
-      artist: "YourBuddyAxel",
-      link: "https://axelspage.carrd.co/",
-    },
-    {
-      thing: "TTS Mascot Design",
-      artist: "PinninaZero",
-      link: "https://www.deviantart.com/pinninazero/about",
-    },
-    {
-      thing: "TTS Mascot Art and Rig",
-      artist: "Omniooman",
-      link: "https://omniooman.uwu.ai/",
-    },
-    {
-      thing: "Stinger",
-      artist: "Kyosaka30",
-      link: "https://kyosakacoms.carrd.co/",
-    },
-    {
-      thing: "Original BGM",
-      artist: "RedSpark_Music",
-      link: "https://redsparkmusic.carrd.co/",
+      title: "Old Credits",
+      credits: [
+        {
+          thing: "1.0 VTuber Model Art + Rig",
+          artists: [
+            { name: "COGNITOHAZARRD", link: "https://linktr.ee/Hackneid" },
+          ],
+        },
+        {
+          thing: "2.0 VTuber Model Art + Rig",
+          artists: [
+            { name: "Qwerty_Soda", link: "https://linktr.ee/qwertysoda" },
+          ],
+        },
+        {
+          thing: "2.0 Model VBridger Enhancements",
+          artists: [
+            { name: "DesdemonaVT", link: "https://twitter.com/DesdemonaVT" },
+          ],
+        },
+        {
+          thing: "Original 3D VTuber Models",
+          artists: [{ name: "Birdchii", link: "https://x.com/birdchii" }],
+        },
+        {
+          thing: "Edits to Original 3D Model",
+          artists: [
+            { name: "Violet_Viper", link: "https://violetviper.carrd.co/" },
+          ],
+        },
+        {
+          thing: "Banner Art",
+          artists: [
+            { name: "dormirVT", link: "https://twitter.com/dormirVT" },
+          ],
+        },
+        {
+          thing: "Emotes",
+          artists: [
+            {
+              name: "EightyEightDesign",
+              link: "https://vgen.co/EightyEightDesign",
+            },
+            {
+              name: "DuggerJuggernaut",
+              link: "https://vgen.co/duggerjuggernaut",
+            },
+            {
+              name: "NocturneAubade",
+              link: "https://nocturneaubade.carrd.co/",
+            },
+            { name: "BeaGremlin", link: "https://beagremlin.carrd.co/" },
+            { name: "GoldCanines", link: "https://goldcanines.carrd.co/" },
+          ],
+        },
+        {
+          thing: "On-Screen Pixel Art",
+          artists: [
+            { name: "InkyFrosty", link: "https://vgen.co/InkyFrosty" },
+            { name: "arealpup", link: "https://vgen.co/arealpup" },
+          ],
+        },
+        {
+          thing: "On-Screen Goober Art",
+          artists: [{ name: "banibani", link: "https://vgen.co/banibani" }],
+        },
+        {
+          thing: "Sub Badges",
+          artists: [
+            {
+              name: "JackGravitty",
+              link: "https://manylink.co/@JackGravitty",
+            },
+          ],
+        },
+        {
+          thing: "Panel Art",
+          artists: [
+            { name: "AmaiMilk", link: "https://twitter.com/AmaiMilk69" },
+          ],
+        },
+        {
+          thing: "Original Logo",
+          artists: [
+            { name: "Baebiiverse", link: "https://www.fiverr.com/baebiiverse" },
+          ],
+        },
+        {
+          thing: "Keychain and Sticker Art",
+          artists: [
+            { name: "kd_0510", link: "https://kdtk0510.wixsite.com/mysite" },
+          ],
+        },
+        {
+          thing: "Chatting and Gaming Background",
+          artists: [
+            { name: "yuzuyukihira", link: "https://vgen.co/yuzuyukihira" },
+          ],
+        },
+        {
+          thing: "Starting and Ending Screens Art",
+          artists: [{ name: "nostdoro", link: "https://nostee.com/" }],
+        },
+        {
+          thing: "Starting and Ending Screens Animation",
+          artists: [
+            {
+              name: "ClericalEric",
+              link: "https://www.twitch.tv/clericaleric",
+            },
+          ],
+        },
+        {
+          thing: "Be Right Back Screen",
+          artists: [
+            { name: "YourBuddyAxel", link: "https://axelspage.carrd.co/" },
+          ],
+        },
+        {
+          thing: "TTS Mascot Design",
+          artists: [
+            {
+              name: "PinninaZero",
+              link: "https://www.deviantart.com/pinninazero/about",
+            },
+          ],
+        },
+        {
+          thing: "TTS Mascot Art and Rig",
+          artists: [{ name: "Omniooman", link: "https://omniooman.uwu.ai/" }],
+        },
+        {
+          thing: "Stinger",
+          artists: [
+            { name: "Kyosaka30", link: "https://kyosakacoms.carrd.co/" },
+          ],
+        },
+        {
+          thing: "Original BGM",
+          artists: [
+            {
+              name: "RedSpark_Music",
+              link: "https://redsparkmusic.carrd.co/",
+            },
+          ],
+        },
+      ],
     },
   ];
 
   return (
-    <div className="flex flex-col items-center h-screen">
-      <h1 className="text-4xl font-bold text-white uppercase mt-8">
-        Art Credits
-      </h1>
-      <div className="mt-4">
-        {artCredits.map((credit, index) => (
-          <div
-            key={credit.thing + index}
-            className="flex flex-col items-center"
-          >
-            <h2 className="text-2xl font-bold text-white">
-              {credit.thing} by{" "}
-              <a
-                href={credit.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-xl text-pink-400 hover:underline hover:text-yellow-400"
+    <div className="flex flex-col items-center pb-8">
+      {creditSections.map((section) => (
+        <div
+          key={section.title}
+          className="flex flex-col items-center w-full"
+        >
+          <h1 className="text-4xl font-bold text-white uppercase mt-8">
+            {section.title}
+          </h1>
+          <div className="mt-4">
+            {section.credits.map((credit, index) => (
+              <div
+                key={credit.thing + index}
+                className="flex flex-col items-center"
               >
-                {credit.artist}
-              </a>
-            </h2>
+                <h2 className="text-2xl font-bold text-white text-center">
+                  {credit.thing} by{" "}
+                  {credit.artists.map((artist, artistIndex) => (
+                    <span key={artist.name + artistIndex}>
+                      {artistIndex > 0 &&
+                        (artistIndex === credit.artists.length - 1
+                          ? " and "
+                          : ", ")}
+                      {artist.link ? (
+                        <a
+                          href={artist.link}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="text-xl text-pink-400 hover:underline hover:text-yellow-400"
+                        >
+                          {artist.name}
+                        </a>
+                      ) : (
+                        <span className="text-xl text-pink-400">
+                          {artist.name}
+                        </span>
+                      )}
+                    </span>
+                  ))}
+                </h2>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
